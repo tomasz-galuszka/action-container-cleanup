4e551bccb4780388ea3daf17fa8ace83fe53ccda
Tomasz Gałuszka <tomasz.g@yggdrasilgaming.com>
2021-11-23 13:15:52 +0100

    next

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 async function run() {
-  const token = core.getInput('token')
-  const packagename = core.getInput('packagename')
-  const octokit = github.getOctokit(token)
-
-  const response = await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByAuthenticatedUser({
-    package_type: 'container',
-    package_name: packagename
-  })
-
-  console.log(response)
+  try {
+    const token = core.getInput('token')
+    const packagename = core.getInput('packagename')
+    const octokit = github.getOctokit(token)
+    const response = await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByAuthenticatedUser({
+      package_type: 'container',
+      package_name: packagename
+    })
+    console.log(response)
+  } catch (error) {
+    console.log('Error occurred')
+    console.log(error.message)
+  }
 }
 run()
